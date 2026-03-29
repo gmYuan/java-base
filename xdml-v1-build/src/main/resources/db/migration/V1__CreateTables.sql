40f8bea987afa6dfbb621284b0bb01d2feea175c
ygm <ygm121351@163.com>
2026-03-30 00:35:20 +0800

    base-done-v1

--- xdml-v1-build/src/main/resources/db/migration/V1__CreateTables.sql
+++ xdml-v1-build/src/main/resources/db/migration/V1__CreateTables.sql
@@ -1,19 +1,19 @@
-create table "USER"(
+create table `USER`(
     id   bigint primary key auto_increment,
     name varchar(100)
 );
 
-create table "MATCH"(
+create table `MATCH`(
     id      bigint primary key auto_increment,
     user_id bigint,
     score int
 );
 
-insert into "USER"(id, name) values (1, 'AAA');
-insert into "USER"(id, name) values (2, 'BBB');
-insert into "USER"(id, name) values (3, 'CCC');
+insert into `USER`(id, name) values (1, 'AAA');
+insert into `USER`(id, name) values (2, 'BBB');
+insert into `USER`(id, name) values (3, 'CCC');
 
-insert into "MATCH"(id, user_id, score) values (1, 1, 1000);
-insert into "MATCH"(id, user_id, score) values (2, 1, 2000);
-insert into "MATCH"(id, user_id, score) values (3, 2, 500);
-insert into "MATCH"(id, user_id, score) values (4, 3, 300);
+insert into `MATCH`(id, user_id, score) values (1, 1, 1000);
+insert into `MATCH`(id, user_id, score) values (2, 1, 2000);
+insert into `MATCH`(id, user_id, score) values (3, 2, 500);
+insert into `MATCH`(id, user_id, score) values (4, 3, 300);
